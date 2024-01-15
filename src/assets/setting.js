d0010256e904ff4ae7f0523985bed66d89645dc4
licklick123123 <156678365+licklick123123@users.noreply.github.com>
2024-01-15 22:35:46 +0800

    Create setting.js

---
+++ src/assets/setting.js
@@ -0,0 +1,17 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: ["default", "rubcard"],
+    collisionMatrix: [[true, false], [false, false]],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Scene/HotUpdate.fire",
+    orientation: "portrait",
+    jsList: ["assets/Script/common/md5/md5.58dd0.js", "assets/Script/common/tools/fixed-render-flow.1beee.js", "assets/Script/components/game/liveVideo/wp.0e3cb.js", "assets/blackjackpvp/script/network/pb/protobuf.cdc6a.js", "assets/mtt/script/common/pb/protobuf.cdc6a.js", "assets/pkfChat/script/network/pb/protobuf.cdc6a.js"],
+    bundleVers: {
+        internal: "01588",
+        resources: "fe7bd",
+        main: "91a46"
+    }
+};
